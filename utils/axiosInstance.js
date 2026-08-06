e35069e5b375343a938f67fab8c8ed65916cfe15
Emmanuel Andy Eze <emmanuel.andyeze@gmail.com>
2026-08-06 11:22:00 +0100

    chore: update production API URL, configure EAS updates, and add project build scripts

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const BASE_URL =
-	process.env.EXPO_PUBLIC_API_URL || 'http://127.0.0.1:8080/api/v1/';
+	process.env.EXPO_PUBLIC_API_URL || 'https://tradeet-server.onrender.com/api/v1/';
 
 const axiosInstance = axios.create({
 	baseURL: BASE_URL,
